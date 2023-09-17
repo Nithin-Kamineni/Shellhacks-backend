0142ff155d95897ddda0502a415d212db36e186d
shamithanarra <shamithanarra@Shamithas-MacBook-Air.local>
2023-09-16 23:26:22 -0400

    persecond

--- VideoSummaryAPI.py
+++ VideoSummaryAPI.py
@@ -1,8 +1,10 @@
 import psycopg2
 from psycopg2 import sql
 from flask import Flask, jsonify, request
+# from flask_cors import CORS
 
 app = Flask(__name__)
+# CORS(app)
 
 db_params = {
         'password': '1234',
