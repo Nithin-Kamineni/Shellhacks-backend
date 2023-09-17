e1503c682fc5e650d9e5856f1d77edb02e9eb539
shamithanarra <shamithanarra@Shamithas-MacBook-Air.local>
2023-09-16 23:11:31 -0400

    per second data

--- VideoSummaryAPI.py
+++ VideoSummaryAPI.py
@@ -46,6 +46,32 @@ def get_timelines_data():
     results = cur.fetchall()
     return {"starting_time":results[0][0], "ending_time":results[0][1]}
 
+#http://127.0.0.1:8080/api/timepersecond?start_timestamp=2023-09-11%2012:00:00&end_timestamp=2023-09-11%2023:00:00
+#http://127.0.0.1:8080/api/timepersecond?start_timestamp=2023-09-11%2012:00:00&end_timestamp=2023-09-11%2023:00:00
+@app.route('/api/timepersecond', methods=['GET'])
+def get_timelines_data_per_second():
+    start_timestamp = request.args.get('start_timestamp')
+    end_timestamp = request.args.get('end_timestamp')
+    cur.execute(f"SELECT column_name FROM information_schema.columns WHERE table_name = 'scene_table';")
+
+    # Fetch all the column names
+    columns = cur.fetchall()
+
+    if(start_timestamp==None or end_timestamp==None):
+        query = sql.SQL('SELECT distinct(SUBSTRING(cast(timestamps as varchar), 12,8)) as time_data_stamp,sum(people::integer) as people,sum(cars::integer) as cars,sum(buildings::integer) as buildings,sum(handbags::integer) as handbags,sum(umbrellas::integer) as umbrellas, MAX(img_url) as img_str FROM scene_table group by distinct(SUBSTRING(cast(timestamps as varchar), 12,8))')
+        cur.execute(query)
+    else:
+        query = sql.SQL('SELECT distinct(SUBSTRING(cast(timestamps as varchar), 12,8)) as time_data_stamp,sum(people::integer) as people,sum(cars::integer) as cars,sum(buildings::integer) as buildings,sum(handbags::integer) as handbags,sum(umbrellas::integer) as umbrellas, MAX(img_url) as img_str FROM scene_table WHERE timestamps >= %s AND timestamps <= %s group by distinct(SUBSTRING(cast(timestamps as varchar), 12,8)) order by time_data_stamp')
+        cur.execute(query,[start_timestamp,end_timestamp])
+    results = cur.fetchall()
+    response=[]
+    for row in results:
+        record={}
+        for col, val in enumerate(row):
+            record[columns[col][0]]=val
+        response.append(record)
+    return jsonify(response) 
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
     cur.close()
